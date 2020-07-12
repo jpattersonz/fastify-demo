54d0944423876d14cfd1614c93d82de7505b1474
jaydhelton <jaydhelton@gmail.com>
2020-07-11 23:36:57 -0400

    swagger

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
 import fastify from 'fastify';
 import autoLoad from 'fastify-autoload';
+import swagger from 'fastify-swagger';
 import * as path from 'path';
 
 const server = fastify({ logger: true });
 
+server.register(swagger, {
+  swagger: {
+    info: {
+      title: 'Test swagger',
+      description: 'testing the fastify swagger api',
+      version: '0.1.0',
+    },
+  },
+  exposeRoute: true,
+  routePrefix: '/documentation',
+});
+
 // Autoload is more powerful than im using it for.
 // this is only autoloading the root module, then ther rest are registered manually
 server.register(autoLoad, {
-  dir: path.join(__dirname, 'api')
+  dir: path.join(__dirname, 'api'),
 });
 
 server.listen(3000, (err, address) => {
