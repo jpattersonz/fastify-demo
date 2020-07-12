a50dc0db3f13c63b0b7c229060aeb6e26f67891f
jaydhelton <jaydhelton@gmail.com>
2020-07-11 23:42:33 -0400

    cool class-validator

--- src/api/v1/people/dto.ts
+++ src/api/v1/people/dto.ts
@@ -1,15 +1,22 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { validationMetadatasToSchemas } from 'class-validator-jsonschema';
 
 export class CreatePersonDto {
-  id: string;
+  @IsString()
+  @IsOptional()
   firstName: string;
 
   @IsNotEmpty()
   @IsString()
   lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @IsString()
+  email: string
 }
 
 // Create JsonSchema from the TS DTO
 // If there are multiple classes in the file, itll on this object
 export const createPersonSchema = validationMetadatasToSchemas().CreatePersonDto;
+console.log(createPersonSchema);
